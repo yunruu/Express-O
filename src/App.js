6186f0a2cf5d02b1efe3577e987a6d6972f73917
yunruu <yunruuchin@gmail.com>
2022-08-14 22:10:48 +0800

    First commit, basic layout of app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddTask from './components/AddTask';
+// import { collection, query, onSnapshot, doc, updateDoc, deleteDoc, } from "firebase/firestore";
+// import { db } from "./firebase";
+// import { toHaveFocus } from '@testing-library/jest-dom/dist/matchers';
+import Sidebar from './components/Sidebar';
+import Main from './components/Main';
+import Calendar from './components/Calendar';
+import Categories from './components/Categories';
+import EditTodo from './components/EditTodo';
+import User from './components/User';
+import TodoList from './components/TodoList';
+import Logout from './components/Logout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Sidebar>
+        <User />
+        <AddTask />
+        <Calendar />
+        <Categories />
+        <Logout />
+      </Sidebar>
+
+      <Main>
+        <TodoList />
+        <EditTodo />
+      </Main>
     </div>
-  );
+  )
 }
 
 export default App;
